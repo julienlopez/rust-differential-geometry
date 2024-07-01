0c03b45c8aed6a53a638191548fbf537dc3c710a
Julien Lopez <julien.lopez51@gmail.com>
2024-07-01 08:02:54 +0200

    finishing the torus embedding

--- tests/test_torus.rs
+++ tests/test_torus.rs
@@ -7,41 +7,94 @@ use rust_differential_geometry::surface::Surface;
 fn it_adds_two() {
     let torus = Surface::from_embedding(
         HashSet::from(['u', 'v']),
-        vec![Expression::Operation {
-            operation: BinaryOperation::Multiplication,
-            left_value: Box::new(Expression::Function {
-                function: Function::Cosine,
-                expression: Box::new(Expression::Monomial {
-                    factor: 1.,
-                    variable: 'u',
-                    power: 1,
-                }),
-            }),
-            right_value: Box::new(Expression::Operation {
-                operation: BinaryOperation::Addition,
-                left_value: Box::new(Expression::Monomial {
-                    factor: 1.,
-                    variable: 'R',
-                    power: 1,
-                }),
-                right_value: Box::new(Expression::Operation {
-                    operation: BinaryOperation::Multiplication,
-                    left_value: Box::new(Expression::Monomial {
+        vec![
+            Expression::Operation {
+                operation: BinaryOperation::Multiplication,
+                left_value: Box::new(Expression::Function {
+                    function: Function::Cosine,
+                    expression: Box::new(Expression::Monomial {
                         factor: 1.,
-                        variable: 'r',
+                        variable: 'u',
                         power: 1,
                     }),
-                    right_value: Box::new(Expression::Function {
-                        function: Function::Cosine,
-                        expression: Box::new(Expression::Monomial {
+                }),
+                right_value: Box::new(Expression::Operation {
+                    operation: BinaryOperation::Addition,
+                    left_value: Box::new(Expression::Monomial {
+                        factor: 1.,
+                        variable: 'R',
+                        power: 1,
+                    }),
+                    right_value: Box::new(Expression::Operation {
+                        operation: BinaryOperation::Multiplication,
+                        left_value: Box::new(Expression::Monomial {
                             factor: 1.,
-                            variable: 'v',
+                            variable: 'r',
                             power: 1,
+                        }),
+                        right_value: Box::new(Expression::Function {
+                            function: Function::Cosine,
+                            expression: Box::new(Expression::Monomial {
+                                factor: 1.,
+                                variable: 'v',
+                                power: 1,
+                            }),
                         }),
                     }),
                 }),
-            }),
-        }],
+            },
+            Expression::Operation {
+                operation: BinaryOperation::Multiplication,
+                left_value: Box::new(Expression::Function {
+                    function: Function::Cosine,
+                    expression: Box::new(Expression::Monomial {
+                        factor: 1.,
+                        variable: 'u',
+                        power: 1,
+                    }),
+                }),
+                right_value: Box::new(Expression::Operation {
+                    operation: BinaryOperation::Addition,
+                    left_value: Box::new(Expression::Monomial {
+                        factor: 1.,
+                        variable: 'R',
+                        power: 1,
+                    }),
+                    right_value: Box::new(Expression::Operation {
+                        operation: BinaryOperation::Multiplication,
+                        left_value: Box::new(Expression::Monomial {
+                            factor: 1.,
+                            variable: 'r',
+                            power: 1,
+                        }),
+                        right_value: Box::new(Expression::Function {
+                            function: Function::Sine,
+                            expression: Box::new(Expression::Monomial {
+                                factor: 1.,
+                                variable: 'v',
+                                power: 1,
+                            }),
+                        }),
+                    }),
+                }),
+            },
+            Expression::Operation {
+                operation: BinaryOperation::Multiplication,
+                left_value: Box::new(Expression::Monomial {
+                    factor: 1.,
+                    variable: 'r',
+                    power: 1,
+                }),
+                right_value: Box::new(Expression::Function {
+                    function: Function::Sine,
+                    expression: Box::new(Expression::Monomial {
+                        factor: 1.,
+                        variable: 'v',
+                        power: 1,
+                    }),
+                }),
+            },
+        ],
     );
     assert_eq!(torus.surface_variables, HashSet::from(['u', 'v']));
     assert_eq!(torus.parametric_variables, HashSet::from(['r', 'R']));
